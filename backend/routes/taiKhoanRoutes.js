ca20541f1f08b70b85241316170b3a02f0c9456e
hungkhuong21 <hungkhuong32@gmail.com>
2025-11-18 10:04:47 +0700

    cập nhật taiKhoanRoutes.js

--- backend/routes/taiKhoanRoutes.js
+++ backend/routes/taiKhoanRoutes.js
@@ -6,6 +6,10 @@ router.get('/', taiKhoanController.getAllTaiKhoan);
 router.get('/:id', taiKhoanController.getTaiKhoanById);
 router.post('/register', taiKhoanController.createTaiKhoan);
 router.post('/login', taiKhoanController.loginTaiKhoan);
+// đặt lại mật khẩu
+router.post('/password-reset/request', taiKhoanController.requestPasswordReset);
+router.post('/password-reset/verify', taiKhoanController.verifyOtp);
+router.post('/password-reset/reset', taiKhoanController.resetPassword);
 router.put('/:id', taiKhoanController.updateTaiKhoan);
 router.delete('/:id', taiKhoanController.deleteTaiKhoan);
 
