a25bdb00c4eef4eb7bbb24036f3285d47e3adf9d
Hinninn <ntlehuyen2k4@gmail.com>
2025-12-30 14:05:13 +0700

    feat: Thêm chức năng tìm kiếm tiếp nhận theo mã lịch hẹn, số điện thoại, cccd

--- backend/routes/tiepNhanRoutes.js
+++ backend/routes/tiepNhanRoutes.js
@@ -5,4 +5,7 @@ const tiepNhanController = require('../controllers/tiepNhanController');
 // Tiếp nhận theo lịch (từ lịch hẹn đã có)
 router.post('/theo-lich', tiepNhanController.tiepNhanTheoLich);
 
+// Tìm kiếm theo mã lịch hẹn, số điện thoại, cccd
+router.get('/search', tiepNhanController.searchTiepNhan);
+
 module.exports = router;
